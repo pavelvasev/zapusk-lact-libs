3ff16d1127a1e9647ceeb6517d9a5541ff19ab8d
root <root@horse-11.home>
2020-06-10 12:40:50 +0500

    bugfixes

--- ssh-authorize-key.zdb/apply.sh
+++ ssh-authorize-key.zdb/apply.sh
@@ -5,20 +5,28 @@ source params.sh
 test ! -z "$user"
 test ! -z "$content"
 
-tgt=~$user/.ssh/authorized_keys
+# eval echo ~username некий способ вычислить домашний каталог пользователя
+tgt=$(eval echo "~$user/.ssh/authorized_keys")
 
-if test -f deployed-key; then
-  k = $(cat deployed-key)
-  if test "$k" = "$content" && grep -qxFs "$content" "$tgt"; then
-    echo key already installed
-    exit 0
+if test -f deployed-key; then # тэкс, уже что-то ставили ранее
+  k=$(cat deployed-key)
+  if test "$k" = "$content"; then  # и то что ставили ранее совпадает с запрошенным сейчас
+    if grep -qxFs "$content" "$tgt"; then # и в файле то оно уже есть
+      echo key already installed # ну и хорошо
+      exit 0
+    fi
+    # но в файле этого нет
+  else
+    # то что ставили ранее - не совпадает с тем, что просят сейчас
+    # надобно бы удалить
+    $zdb_dir/destroy.sh
   fi
-  # ключ установлен, да не тот
-  # надобно бы удалить
-  $zdb_dir/destroy.sh
 fi
 
-grep -qxFs "$content" "$tgt" || echo "$content" | tee --append "$tgt"
+# вроде бы как tee --append пишет лучше, потому что мол он в sudp режиме сработает, а >> нет
+# и второй приятный подарок - он как-то разбирается с переводом строк
+# (если файл не кончается переводом строки - он его добавляет, я так вижу сейчас)
+grep -qxFs "$content" "$tgt" || echo "$content" | tee --append "$tgt" >/dev/null
 
 echo key installed
 
--- ssh-authorize-key.zdb/destroy.sh
+++ ssh-authorize-key.zdb/destroy.sh
@@ -5,12 +5,21 @@ if test ! -f deployed-key; then
   exit 0
 fi
 
+source params.sh
+
 k=$(cat deployed-key)
 test ! -z "$user"
-tgt=~$user/.ssh/authorized_keys
+tgt=$(eval echo "~$user/.ssh/authorized_keys")
+
+# ладно уж..
+#deployed_content=$(cat deployed-key)
+#if ! grep -qxFs "$deployed_content" "$tgt"; then
+#  echo "do not see key in $tgt"
+#  exit 0
+#fi
 
 # вытащили все, что не про наш ключ, во времянку
-grep -v "$k" "$tgt" tmpkeys
+grep -v "$k" "$tgt" >tmpkeys
 # скопировали с сохранением прав исходного файла
 cp --preserve tmpkeys "$tgt"
 # удалили времянку
@@ -18,3 +27,5 @@ rm tmpkeys
 
 # ну и удалили память
 rm deployed-key
+
+echo key removed
