895e666f17008794316b248f531959014fb97a40
Pavel Vasev <pavel.vasev@gmail.com>
2020-06-18 02:37:21 +0500

    better

--- xif.zdb/xif.rb
+++ xif.zdb/xif.rb
@@ -22,6 +22,11 @@ end
 s=ENV["day"]||""
 if s.length > 0
   d = s.split(/[\s,]+/).map(&:to_i)
+  
+  # добавим 0 в качестве вс для совместимости с крон, 
+  # ибо руби 1..7 https://apidock.com/ruby/Date/cwday
+  d.push(7) if d.include?(0) 
+  
   today=Date.today.cwday  
   if ! d.include?(today)
     puts "xif: today's day #{today} is not in list of days #{d.inspect}"
