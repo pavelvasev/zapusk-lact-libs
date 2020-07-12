9836679ad319a555a935505cca31f9fe22d95ae1
pavelvasev <pavel.vasev@gmail.com>
2020-07-12 19:04:20 +0000

    swap enhancement

--- swap-setup.zdb/apply.sh
+++ swap-setup.zdb/apply.sh
@@ -2,6 +2,10 @@
 source params.sh
 test ! -z "$size_mb"
 test ! -z "$path"
+if test -f "$path"; then
+  echo "swap file already exist. skipping"
+  exit 0
+fi
 
 dd if=/dev/zero of="$path" bs=1M count="$size_mb" status=progress
 chmod 0600 "$path"
